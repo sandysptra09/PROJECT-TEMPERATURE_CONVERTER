2c6fa971ea7dc00928a6706175319ccae0257fad
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2023-11-09 14:08:56 +0700

    Create pages About

---
+++ src/views/About.jsx
@@ -0,0 +1,78 @@
+import "./Konversi.css";
+
+export default function About() {
+    return (
+        <>
+            <h2>Konversi Suhu</h2>
+            <div className="about-convert">
+                <p>
+                    Satuan suhu yang terkenal adalah Celcius (°C), Kelvin (K), Fahrenheit
+                    (°F), Reamur (°R) dan Rankine (Ra).
+                </p>
+                <p>
+                    WMO [2] menggunakan satuan derajat Celcius sebagai satuan suhu yang
+                    berkaitan dengan kegiatan meteorologi atau informasi cuaca. Suhu udara
+                    juga merupakan parameter utama untuk menggambarkan terjadinya
+                    perubahan iklim.{" "}
+                </p>
+                <p>
+                    Amerika menggunakan satuan Fahrenheit untuk suhu udara. Adapun media
+                    di Inggris memiliki konsensus menggunakan dua satuan suhu udara yaitu
+                    Celcius dan Fahrenheit.
+                </p>
+                <p>
+                    Kelvin digunakan sebagai "Standar Internasional"dalam kepentingan
+                    sains.
+                </p>
+                <p>
+                    Adanya perbedaan satuan suhu udara yang digunakan maka diperlukan
+                    konversi suhu online dari dan ke satuan Celcius, Kelvin, Fahrenheit,
+                    Reamur dan Rankine sebagai alat bantu yang memudahkan.
+                </p>
+            </div>
+
+            <h2>Konsep Suhu</h2>
+            <div className="konsep-suhu">
+                <p>
+                    Suhu merupakan nilai yang digunakan untuk menunjukkan derajat panas
+                    suatu benda. Suhu juga akan menentukan wujud suatu zat apakah akan
+                    berbentuk cair, padat atau gas.
+                </p>
+                <p>
+                    Misalnya air pada suhu di bawah titik bekunya (0 °C) akan berwujud
+                    beku dan di atas titik didihnya (100 °C) akan berwujud gas. Besaran
+                    titik beku dan titik didih setiap zat berbeda-beda. Dalam konsep
+                    termodinamika, suhu merupakan ukuran karaktersitik fisik dari
+                    gerak-gerak acak molekul-molekul yang ada dalam suatu benda.
+                </p>
+                <p>
+                    Semakin tinggi suhu suatu benda maka semakin cepat gerak
+                    molekul-molekulnya. Pada saat suhu mencapai nol mutlak yaitu 0 K, maka
+                    dinyatakan molekul-molekul menjadi diam, tidak memiliki energi
+                    kinetik.
+                </p>
+                <p>
+                    Benda yang melepaskan panas berarti memiliki suhu lebih tinggi dan
+                    benda yang menerima panas berarti memiliki suhu yang lebih rendah.
+                </p>
+                <p>
+                    Secara kualitatif kita dapat menyatakan suatu benda panas jika suhunya
+                    lebih tinggi dari yang biasa kita rasakan dan sebaliknya kita
+                    menyatakan dingin jika suhu benda tersebut lebih rendah dari yang
+                    biasa kita rasakan.
+                </p>
+                <p>
+                    Untuk menyatakan besaran suhu yang lebih teliti maka digunakan ukuruan
+                    kuantitatif menggunakan termometer. Ukuran suhu secara kuantitatif
+                    dinyatakan dalam beberapa satuan yaitu Celcius, Kelvin, Fahrenheit,
+                    Reamur. Masing-masing satuan suhu memiliki range skalanya tersendiri.
+                </p>
+                <p>
+                    Dengan perbedaan satuan suhu udara yang berlaku pada masing-masing
+                    negara maka konversi suhu online dapat menjadi alat bantu yang
+                    praktis.
+                </p>
+            </div>
+        </>
+    );
+}
--- src/views/Konversi.jsx
+++ src/views/Konversi.jsx
@@ -1,8 +1,12 @@
+import About from "./About";
+
 export default function Konversi() {
     return (
         <div>
             <h1>Konversi Suhu</h1>
             <img src="/images-views/temperature.png" alt="Logo Konversi Suhu" />
+
+            <About/>
         </div>
     )
 }
